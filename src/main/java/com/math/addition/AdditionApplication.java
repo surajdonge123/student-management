d136e71b1a5b7c77884225b6b0cf83435bca1fa9
Suraj Donge <surajdonge143@gmail.com>
2023-04-18 16:29:14 +0530

    Thids is commit by Suraj

---
+++ src/main/java/com/math/addition/AdditionApplication.java
@@ -0,0 +1,13 @@
+package com.math.addition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdditionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdditionApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/math/addition/Sum/Sum.java
@@ -0,0 +1,15 @@
+package com.math.addition.Sum;
+
+public class Sum {
+
+	public int sum(int a,int b) {
+		
+		
+		int c=a+b;
+		System.out.println("The addition of this two NUmber is "+c);
+
+		return c;
+		
+	}
+	
+}
